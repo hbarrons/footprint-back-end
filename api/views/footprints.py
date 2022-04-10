b10abe8c4cd7190d8990901b200934468b4a747d
hbarrons <hbarrons@gmail.com>
2022-04-10 19:05:22 -0400

    make successful third party api call

--- api/views/footprints.py
+++ api/views/footprints.py
@@ -1,18 +1,29 @@
-from flask import Blueprint, jsonify, request
+from flask import Flask, Blueprint, jsonify, request
 from api.middleware import login_required, read_token
+import os
+import urllib.request, json
 
 from api.models.db import db
 from api.models.footprint import Footprint
 
+API_KEY = os.getenv('API_KEY')
+
 footprints = Blueprint('footprints', 'footprints')
 
-@footprints.route('/', methods=["POST"])
-@login_required
-def create():
-  data = request.get_json()
-  profile = read_token(request)
-  data["profile_id"] = profile["id"]
-  footprint = Footprint(**data)
-  db.session.add(footprint)
-  db.session.commit()
-  return jsonify(footprint.serialize()), 201
+@footprints.route('/', methods=["GET"])
+# @login_required
+def getFootprint():
+  url = f"https://klimaat.app/api/v1/calculate?start=48208&end=90210&transport_mode=driving&num_passengers=1{API_KEY}"
+  print(url)
+  response = urllib.request.urlopen(url)
+  data = response.read()
+  print(data)
+  # data = request.get_json()
+  # profile = read_token(request)
+  # data["profile_id"] = profile["id"]
+  # footprint = Footprint(**data)
+  # db.session.add(footprint)
+  # db.session.commit()
+  # return jsonify(footprint.serialize()), 201
+
+getFootprint()
